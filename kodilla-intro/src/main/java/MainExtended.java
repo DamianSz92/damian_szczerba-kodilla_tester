cc0b3f1e68077bc140e3bdc1437c2d15b4c236a6
Damian Szczerba <damianszczerba92@gmail.com>
2022-11-27 13:26:55 +0100

    Zadanie nr 2 - tworzenie zmiennych

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,13 @@
+public class MainExtended {
+    public static void main(String[] args){
+        char exampleID = 'A';
+        String nameOfFigure = "Prostokąt";
+        double sideLenghtA = 4.12;
+        double sideLenghtB = 2.67;
+        boolean istriangle = false;
+        System.out.println("Example ID -" + exampleID + "- Name of figure is - " + nameOfFigure);
+        System.out.println("Side A lenght is - " + sideLenghtA + " - Side B lenght is - " + sideLenghtB);
+        System.out.println("Is triangle? " + istriangle);
+
+    }
+}
